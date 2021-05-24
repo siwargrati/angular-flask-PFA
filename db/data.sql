e627298d2e1eeb1f3607d860646d355c3c148bfc
Siwar grati <49911725+siwargrati@users.noreply.github.com>
2021-05-24 10:02:40 +0100

    third

---
+++ db/data.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE pfa;
+use pfa;
+
+CREATE TABLE `data` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `file` blob NOT NULL,
+
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
